a9b55530e004c3637818506e5126865d57e73191
kdsam <kdmandawe06@gmail.com>
2019-06-28 19:26:01 +0800

    Work-around for SSL

--- src/main/java/com/github/kdsam/learnstorm/ex14_IntegratingStorm/TopologyMain.java
+++ src/main/java/com/github/kdsam/learnstorm/ex14_IntegratingStorm/TopologyMain.java
@@ -1,5 +1,11 @@
 package com.github.kdsam.learnstorm.ex14_IntegratingStorm;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.topology.TopologyBuilder;
@@ -7,6 +13,22 @@ import org.apache.storm.topology.TopologyBuilder;
 public class TopologyMain {
 
     public static void main(String[] args) throws InterruptedException {
+
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager(){
+            public X509Certificate[] getAcceptedIssuers(){return null;}
+            public void checkClientTrusted(X509Certificate[] certs, String authType){}
+            public void checkServerTrusted(X509Certificate[] certs, String authType){}
+        }};
+
+// Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            System.out.println("Exception!!! "  + e.getMessage());
+        }
 
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("tweets-collector", new TwitterSpout());
@@ -17,7 +39,7 @@ public class TopologyMain {
         Config conf = new Config();
         conf.setDebug(true);
         cluster.submitTopology("twitter-direct", conf, builder.createTopology());
-        Thread.sleep(10000);
+        Thread.sleep(40000);
         cluster.shutdown();
     }
 }
